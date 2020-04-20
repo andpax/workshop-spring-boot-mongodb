dbb0b14d84a578d5dac354916a99bbf2e22c6092
Anderson Nascimento <and.pax@gmail.com>
2020-04-20 15:54:40 -0300

    Inserção de usuário com POST

--- src/main/java/com/andpax/workshopmongo/services/UserService.java
+++ src/main/java/com/andpax/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.andpax.workshopmongo.domain.User;
+import com.andpax.workshopmongo.dto.UserDTO;
 import com.andpax.workshopmongo.repository.UserRepository;
 import com.andpax.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,6 +25,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User inser(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
