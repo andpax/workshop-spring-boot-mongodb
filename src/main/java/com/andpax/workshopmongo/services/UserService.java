af75ef4f9229c1cdc33f1f8eb0e01d0ce762f902
Anderson Nascimento <and.pax@gmail.com>
2020-04-20 15:17:22 -0300

    Obtendo um usuário por id

--- src/main/java/com/andpax/workshopmongo/services/UserService.java
+++ src/main/java/com/andpax/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.andpax.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.andpax.workshopmongo.domain.User;
 import com.andpax.workshopmongo.repository.UserRepository;
+import com.andpax.workshopmongo.services.exception.ObjectNotFoundException;
 
 
 @Service
@@ -17,6 +19,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
