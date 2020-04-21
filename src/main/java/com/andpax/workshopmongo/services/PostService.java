be9baff98e18392c8d152f1e69a6473b9138a00f
Anderson Nascimento <and.pax@gmail.com>
2020-04-21 14:04:56 -0300

    Obtendo um post por id

---
+++ src/main/java/com/andpax/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.andpax.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andpax.workshopmongo.domain.Post;
+import com.andpax.workshopmongo.repository.PostRepository;
+import com.andpax.workshopmongo.services.exception.ObjectNotFoundException;
+
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
