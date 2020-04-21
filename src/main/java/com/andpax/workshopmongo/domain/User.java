5f1862ac739b7f939fad3aba7dcb0071a6f0d0a7
Anderson Nascimento <and.pax@gmail.com>
2020-04-21 13:38:16 -0300

    Referenciando os posts do usuário

--- src/main/java/com/andpax/workshopmongo/domain/User.java
+++ src/main/java/com/andpax/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.andpax.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
@@ -15,6 +18,9 @@ public class User implements Serializable {
 	
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> post = new ArrayList<>();
 	
 	public User() {
 		// TODO Auto-generated constructor stub
@@ -48,6 +54,14 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	public List<Post> getPost() {
+		return post;
+	}
+
+	public void setPost(List<Post> post) {
+		this.post = post;
 	}
 
 	@Override
